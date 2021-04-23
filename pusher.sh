11ce0762d922b2a171f90d3026f03e705847bc9f
Krzysztof Krason <Krzysztof.Krason@gmail.com>
2021-04-23 17:37:18 +0200

    update

--- pusher.sh
+++ pusher.sh
@@ -4,15 +4,15 @@ set -x
 
 CLONE_DIR=$(mktemp -d)
 
-echo "Cloning destination git repository"
-git clone --single-branch --branch main "https://szczepienia:$PAT@github.com/szczepienia/szczepienia.github.io.git" "$CLONE_DIR"
-
 echo "Building"
 mvn -B clean package -DskipTests -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn
 
 echo "Generating new data"
-#java -jar target/szczepimy-1.0-SNAPSHOT.jar -p $EREJ_PID_PLUTA -s $EREJ_SID -c $EREJ_CSRF -t $CLONE_DIR -v OPOLSKIE
+java -jar target/szczepimy-1.0-SNAPSHOT.jar -p $EREJ_PID_PLUTA -s $EREJ_SID -c $EREJ_CSRF -t $CLONE_DIR -v OPOLSKIE
 
+
+echo "Cloning destination git repository"
+git clone --single-branch --branch main "https://szczepienia:$PAT@github.com/szczepienia/szczepienia.github.io.git" "$CLONE_DIR"
 cd "$CLONE_DIR"
 
 echo "Adding git commit"
