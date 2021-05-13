d5642d97af92f77853b193b83350dd4e2636be93
Krzysztof Krason <Krzysztof.Krason@gmail.com>
2021-05-13 16:36:03 +0200

    ignore search

--- src/main/java/com/kirela/szczepimy/BasicSlotWithSearch.java
+++ src/main/java/com/kirela/szczepimy/BasicSlotWithSearch.java
@@ -1,6 +1,7 @@
 package com.kirela.szczepimy;
 
 import java.time.Instant;
+import java.util.Objects;
 import java.util.UUID;
 
 public record BasicSlotWithSearch(
@@ -20,5 +21,29 @@ public record BasicSlotWithSearch(
             slot.mobility(),
             srh
         );
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this)
+            return true;
+        if (obj == null || obj.getClass() != this.getClass())
+            return false;
+        var that = (BasicSlotWithSearch) obj;
+        return Objects.equals(this.id, that.id) &&
+            Objects.equals(this.startAt, that.startAt) &&
+            this.duration == that.duration &&
+            Objects.equals(this.servicePoint, that.servicePoint) &&
+            Objects.equals(this.vaccineType, that.vaccineType) &&
+            this.dose == that.dose &&
+            Objects.equals(this.status, that.status) &&
+            Objects.equals(this.mobility, that.mobility);
+        // ignore search
+    }
+
+    @Override
+    public int hashCode() {
+        // ignore search
+        return Objects.hash(id, startAt, duration, servicePoint, vaccineType, dose, status, mobility);
     }
 }
